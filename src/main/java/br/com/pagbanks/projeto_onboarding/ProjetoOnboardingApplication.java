d647512d855cccb49f873c0a5a1448a63f0f6224
andrecabalo <andreccabalo@gmail.com>
2024-08-06 12:13:21 -0300

    subindo estrutura criada no spring.io

---
+++ src/main/java/br/com/pagbanks/projeto_onboarding/ProjetoOnboardingApplication.java
@@ -0,0 +1,13 @@
+package br.com.pagbanks.projeto_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoOnboardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pagbanks/projeto_onboarding/ProjetoOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pagbanks.projeto_onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
