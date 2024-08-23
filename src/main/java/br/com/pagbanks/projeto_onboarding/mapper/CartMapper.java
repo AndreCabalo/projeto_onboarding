9a2592fd72ae1b1b1c93c6fe2f7c4bd54f03e67e
andrecabalo <andreccabalo@gmail.com>
2024-08-23 17:44:15 -0300

    Adicionado mapper para conversao entre DTO e Entity

---
+++ src/main/java/br/com/pagbanks/projeto_onboarding/mapper/CartMapper.java
@@ -0,0 +1,17 @@
+package br.com.pagbanks.projeto_onboarding.mapper;
+
+import br.com.pagbanks.projeto_onboarding.dto.CartDto;
+import br.com.pagbanks.projeto_onboarding.entity.Cart;
+
+public class CartMapper {
+
+    public static CartDto toCartDtoFrom(Cart cart) {
+        return new CartDto(cart.getId(), cart.getListItens(), cart.getCreationDate(), cart.getTotalValue());
+    }
+
+
+    public static Cart toCartFrom(CartDto cartDto) {
+        return new Cart(cartDto.id(),cartDto.listItens(),cartDto.creationDate(),cartDto.totalValue());
+
+    }
+}
