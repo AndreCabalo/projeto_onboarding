33cab0dc4e02a7fa16f1e1c447875102fb3678de
andrecabalo <andreccabalo@gmail.com>
2024-08-27 11:23:01 -0300

    adicionando o enableJpaAuditing, para o createdDate funcionar

--- src/main/java/br/com/pagbanks/projeto_onboarding/ProjetoOnboardingApplication.java
+++ src/main/java/br/com/pagbanks/projeto_onboarding/ProjetoOnboardingApplication.java
@@ -2,11 +2,13 @@ package br.com.pagbanks.projeto_onboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ProjetoOnboardingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjetoOnboardingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoOnboardingApplication.class, args);
+    }
 }
